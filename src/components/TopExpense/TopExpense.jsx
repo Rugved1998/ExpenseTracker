912bd0121d5b7fa2a8285e6848b3121102b8099d
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-05-19 15:25:06 +0530

    Final Commit

--- src/components/TopExpense/TopExpense.jsx
+++ src/components/TopExpense/TopExpense.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, PieChart, Pie, Cell, Legend } from 'recharts';
 
 
+
 export default function TopExpense  ({ transactions })  {
   const categoryData = transactions.reduce((acc, transaction) => {
     const { category, amount } = transaction;
@@ -14,18 +15,20 @@ export default function TopExpense  ({ transactions })  {
     return acc;
   }, []);
 
-  const COLORS = ['#8884d8', '#82ca9d', '#ffc658', '#ff8042', '#8dd1e1', '#a4de6c', '#d0ed57'];
-
+  
   return (
     <div className="top-expense-section">
       <h2>Top Expenses</h2>
       <div className="chart-container">
         <div className="bar-chart">
-          <BarChart width={600} height={300} data={categoryData}>
-            <XAxis dataKey="category" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="amount" fill="#8884d8" />
+            
+          <BarChart width={600} height={300} data={categoryData} >
+             <XAxis dataKey="category" />
+            {/* <YAxis /> */}
+            {/* <Tooltip /> */}
+            
+             <Bar dataKey="amount" fill="#8884d8" /> 
+            
           </BarChart>
         </div>
       </div>
